23e9f3dd9dca245194b2c6819de7ce8dbafa6e69
Cesar Rivas <crivasg@gmail.com>
2016-09-12 20:50:48 -0500

    Added function to parse a time string variable

--- podcasts.go
+++ podcasts.go
@@ -14,6 +14,7 @@ import (
 	"os/user"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 const (
@@ -192,7 +193,40 @@ func GetFeedList() ([]string, string, error) {
 
 }
 
+// https://github.com/jteeuwen/go-pkg-rss/blob/master/timeparser.go
+func ParseTime(formatted string) (time.Time, error) {
+	var layouts = [...]string{
+		"Mon, _2 Jan 2006 15:04:05 MST",
+		"Mon, _2 Jan 2006 15:04:05 -0700",
+		time.ANSIC,
+		time.UnixDate,
+		time.RubyDate,
+		time.RFC822,
+		time.RFC822Z,
+		time.RFC850,
+		time.RFC1123,
+		time.RFC1123Z,
+		time.RFC3339,
+		time.RFC3339Nano,
+		"Mon, 2, Jan 2006 15:4",
+		"02 Jan 2006 15:04:05 MST",
+	}
+	var t time.Time
+	var err error
+	formatted = strings.TrimSpace(formatted)
+	for _, layout := range layouts {
+		t, err = time.Parse(layout, formatted)
+		if !t.IsZero() {
+			break
+		}
+	}
+	return t, err
+}
+
 func main() {
+
+	t := time.Now()
+	fmt.Println(t.Format(time.RFC1123))
 
 	feed_list, feed_path, _ := GetFeedList()
 	fmt.Println(feed_path)
