1355c7eb626a249060dcb7f0497c87b3ae761a39
Cesar Rivas <crivasg@gmail.com>
2017-03-20 16:31:53 -0600

    Gets the podcast list file.
    If the file does not exist, it will created it with the added feedURL
    
    Working in #3

--- add_url.go
+++ add_url.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/user"
+	"path/filepath"
 )
 
 func addUrl(feedUrl string) error {
@@ -15,8 +17,22 @@ func addUrl(feedUrl string) error {
 		return err
 	}
 
-    // http://stackoverflow.com/a/16785343
+	// http://stackoverflow.com/a/16785343
 	finalURL := res.Request.URL.String()
+	fmt.Fprintf(os.Stdout, "podcasts: %s\n\n", finalURL)
+
+	// get the podcast list file
+	usr, _ := user.Current()
+	feedPath := filepath.Join(usr.HomeDir, ".podcasts", "feeds.txt")
+
+	// if the file doesn't exist, create it with the feedURL string.
+	if _, err := os.Stat(feedPath); os.IsNotExist(err) {
+		err = writeText(feedUrl, feedPath)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
 
 	return nil
 }
